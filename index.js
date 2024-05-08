cce9f959a16c66ab157583356fb27b3c24cca767
tasnimmondoltuba <tasnimtuba744@gmail.com>
2024-05-08 20:36:35 +0600

    updated added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,69 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const checkCollection = client.db("carDoctor").collection("checkings");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: {
+          title: 1,
+          price: 1,
+          service_id: 1,
+          img: 1,
+        },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // checkings
+    app.get("/checkings", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await checkCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/checkings", async (req, res) => {
+      const checking = req.body;
+      console.log(checking);
+      const result = await checkCollection.insertOne(checking);
+      res.send(result);
+    });
+    app.patch("/checkings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updatedoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+      const result = await checkCollection.updateOne(filter, updatedoc);
+      res.send(result);
+    });
+
+    app.delete("/checkings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await checkCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +95,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
